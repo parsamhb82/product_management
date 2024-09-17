48f4ee254e9d4136dc11c50cc048cb3b019aa07a
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-17 21:19:07 +0430

    person views.py imports

--- product_management_app/person/views.py
+++ product_management_app/person/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import NaturalPerson, LegalPerson
+from .serializers import NaturalPersonSerializer, LegalPersonSerializer
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
