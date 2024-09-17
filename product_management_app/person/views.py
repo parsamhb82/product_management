d2f985a2ce637ebaa1de52ac7a8e8aa1ff0c3da7
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-17 21:42:27 +0430

    adding person app views

--- product_management_app/person/views.py
+++ product_management_app/person/views.py
@@ -1,5 +1,15 @@
-from rest_framework import viewsets
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from .models import NaturalPerson, LegalPerson
 from .serializers import NaturalPersonSerializer, LegalPersonSerializer
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+class NaturalPersonListCreateView(ListCreateAPIView):
+    queryset = NaturalPerson.objects.all()
+    serializer_class = NaturalPersonSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['wallet']
+    search_fields = ['national_id', 'user__username']
+
+class  NaturalPersonRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = NaturalPerson.objects.all()
+    serializer_class = NaturalPersonSerializer
