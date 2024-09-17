14cf607ab4a23347648cc439f04f98403959aa59
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-17 14:52:05 +0330

    discount serilizer added

--- product_management_app/factor/serializers.py
+++ product_management_app/factor/serializers.py
@@ -4,3 +4,32 @@ class DiscountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discount
         fields = '__all__'
+    
+    def create(self, validated_data):
+        products = validated_data.pop('product', None)
+        users = validated_data.pop('User', None)
+        
+        # Handle all products case
+        if not products:
+            validated_data['all_products'] = True
+        else:
+            validated_data['all_products'] = False
+
+        # Handle all users case
+        if not users:
+            validated_data['all_customers'] = True
+        else:
+            validated_data['all_customers'] = False
+        
+        # Create the discount instance
+        discount = Discount.objects.create(**validated_data)
+        
+        # Assign products if specific products were provided
+        if products:
+            discount.product.set(products)
+        
+        # Assign users if specific users were provided
+        if users:
+            discount.User.set(users)
+        
+        return discount
