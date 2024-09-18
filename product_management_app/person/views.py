bf009b944c4e63aaeca26fce87dae6e4bd8d3ed6
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-18 15:31:39 +0430

    legal person views edited

--- product_management_app/person/views.py
+++ product_management_app/person/views.py
@@ -13,3 +13,14 @@ class NaturalPersonListCreateView(ListCreateAPIView):
 class  NaturalPersonRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = NaturalPerson.objects.all()
     serializer_class = NaturalPersonSerializer
+    
+class LegalPersonListCreateView(ListCreateAPIView):
+    queryset = LegalPerson.objects.all()
+    serializer_class = LegalPersonSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['is_active', 'wallet']
+    search_fields = ['company_name', 'company_id', 'user__username']
+
+class LegalPersonRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = LegalPerson.objects.all()
+    serializer_class = LegalPersonSerializer
