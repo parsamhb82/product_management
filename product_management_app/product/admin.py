478bdd65153dd7b206d76f82921d01cae63a5a53
Your Name <you@example.com>
2024-09-18 08:15:52 -0700

    admin

--- product_management_app/product/admin.py
+++ product_management_app/product/admin.py
@@ -3,4 +3,10 @@ from .models import *
 @admin.register (Product)
 class ProductAdmin(admin.ModelAdmin): ...
 
+
+@admin.register (Discount)
+class DiscounttAdmin(admin.ModelAdmin): ...
+
+@admin.register (Category)
+class ProductAdmin(admin.ModelAdmin): ...
 # Register your models here.
