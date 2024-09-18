940acb7ce6f59ad706ac46cd44f0b3bf01da6497
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-18 20:51:37 +0330

    views completed

--- product_management_app/product/serializers.py
+++ product_management_app/product/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework.serializers import ModelSerializer
 from .models import *
 
-class Productserializer(ModelSerializer):
+class CreateProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['name', 'description', 'price', 'category', 'stock', 'image']
+
+class ProductSerilizer(ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = "__all__"
--- product_management_app/product/views.py
+++ product_management_app/product/views.py
@@ -1,36 +1,52 @@
 from django.shortcuts import render
-from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView , ListAPIView)
+from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView , ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView)
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from .serializers import Productserializer
+from .serializers import *
 from .models import Product
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.permissions import IsAdminUser, IsAuthenticated , AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-class login(TokenObtainPairView):
-    pass
+import uuid
 
 
-class refresh(TokenRefreshView):
-    pass
+def generate_unique_product_code():
+    while True:
+        code = str(uuid.uuid4())[:16]
+        if not Product.objects.filter(code=code).exists():
+            return code
 
-class AddProduct(ListCreateAPIView):
+
+class AddProduct(CreateAPIView):
     queryset =  Product.objects.all()
-    serializer_class = Productserializer
+    serializer_class = CreateProductSerializer
     permission_classes = [IsAdminUser]
     def perform_create(self, serializer):
-        owner = serializer.save(owner = self.request.user)
-        return owner
-class EditProduct(RetrieveUpdateDestroyAPIView):
+        code = generate_unique_product_code()
+        owner = serializer.save(owner = self.request.user.legalperson, code = code)
+        
+class EditProduct(UpdateAPIView):
     queryset =  Product.objects.all()
-    serializer_class = Productserializer
+    serializer_class = CreateProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.filter(owner = self.request.user.legalperson)
+
+class ListProductView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerilizer
     filter_backends = [DjangoFilterBackend , filters.SearchFilter, filters.OrderingFilter]
     ordering_fields = ["price", "stock"]
     search_fields = ["name"]
     filterset_fields = ["category"]
-    permission_classes = [IsAdminUser]
+
+class DestroyProductView(DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerilizer
+    def get_queryset(self):
+        return Product.objects.filter(owner = self.request.user.legalperson)
+
     
     
 # Create your views here.
